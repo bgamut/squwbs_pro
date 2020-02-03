0c1d5e7b8a334f63da915f8ff45d609a159928bf
bgamut <bgamut@gmail.com>
2020-02-04 00:39:49 +0900

    Update squwbs_pro.py

--- Resources/app/squwbs_pro.py
+++ Resources/app/squwbs_pro.py
@@ -1,2 +1,8 @@
 import os
-os.system('../Resources/app/src/assets/node-v13.3.0-darwin-x64 ../Resources/app/starter.js')
+bc=os.path.dirname(os.cwd())+r"/Resources/app/src/node-v13.3.0-darwin-x64"
+command1=bc+" "+os.path.dirname(os.getcwd())+r"/Resources/app/starter.js"
+if os.path.isfile(bc):
+  command=command1
+else:
+  command=os.path.dirname(os.cwd())+r"/app/src/assets/node-v13.3.0-darwin-x64 "+os.path.dirname(os.getcwd())+r"/app/starter.js"
+os.system(command)
