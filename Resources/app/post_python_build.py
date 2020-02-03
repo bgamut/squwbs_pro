00969b40d6b1a127174ea12d7b2022666d33ed9c
bgamut <bgamut@gmail.com>
2020-02-04 02:22:02 +0900

    Update post_python_build.py

--- Resources/app/post_python_build.py
+++ Resources/app/post_python_build.py
@@ -1,12 +1,13 @@
 import subprocess
-rawout=subprocess.check_output(['otool','-L','../../MacOS/squwbs_pro']).splitlines()
+rawout=subprocess.check_output(['otool', '-L', '../../MacOS/squwbs_pro']).splitlines()
 source=[]
 to=[]
 for i in range(len(rawout)):
-    if(i!=0):
+    if(i>0):
         source.append(str(rawout[i]).split(' ')[0][4:])
 for i in range(len(source)):
-    to.append('../../Frameworks/'+source[0].split('/')[-1])
+    #Disable System Integrity Protection (rootless) in Mac OS X for the following command to work
+    to.append('../../Frameworks/'+source[i].split('/')[-1]) 
     subprocess.Popen(['cp',source[i],to[i]])
 for i in range(len(source)):
     subprocess.Popen(['install_name_tool','-change',source[i],'@executable_path'+to[i],'../../MacOS/squwbs_pro'])
