35895961bf4c4a1e5732f3d65c5c1e52d50724f9
B <bgamut@gmail.com>
2020-02-06 10:36:53 +0900

    test directory cleaning

--- Resources/app/tests/library_test/index.js
+++
@@ -1,8 +0,0 @@
-function spline(int32arraybuffer,sampleRate){
-    // const AcceptArrayBuffer= require ('./build/Release/addon.node').AcceptArrayBuffer()
-    // return AcceptArrayBuffer(int32arraybuffer,sampleRate)
-    return "some fries mf"
-}
-module.exports=function(int32arraybuffer,sampleRate){
-    spline(int32arrayBuffer,sampleRate)
-}
